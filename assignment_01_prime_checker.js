40be29777e13bfc93b040e4e8a6f3e929f022810
ITSJAQ <jonhquaye416@gmail.com>
2026-08-07 01:24:13 +0000

    Completed prime checker assignment

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -43,3 +43,41 @@
 // =============================================================================
 
 
+
+function isPrime(number)
+{
+    // Numbers less than 2 are not prime
+    if (number < 2)
+    {
+        return false;
+    }
+
+    // Check for divisors
+    for (let i = 2; i < number; i++)
+    {
+        if (number % i === 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function main()
+{
+    const readlineSync = require("readline-sync");
+
+    let number = readlineSync.questionInt("Enter a number: ");
+
+    if (isPrime(number))
+    {
+        console.log(number + " is a prime number.");
+    }
+    else
+    {
+        console.log(number + " is NOT a prime number.");
+    }
+}
+
+main();
